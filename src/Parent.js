3940707ddac666ef3ef706c78f73e7c1ea7927df
Alicia <arrowsrp@yahoo.com>
2022-02-27 15:11:21 -0800

    commit tree f135f930a9630ede901a61eb611de1e522a7c367

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,14 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  function onChangeColor (){
+    setColor(randomColor);
+  }
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={onChangeColor} />
+      <Child onChangeColor={onChangeColor} />
     </div>
   );
 }
